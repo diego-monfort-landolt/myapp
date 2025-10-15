f457a4048fad710bcf65277a8d556015dd0c6a31
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-10-15 15:00:56 +0200

    hinzufügen verschidener componente

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { TitleComponent } from "../title/title.component";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  imports: [TitleComponent,]
+})
+export class HeaderComponent {
+  pageTitle = 'My Application Header';
+  constructor() {}
+
+}
