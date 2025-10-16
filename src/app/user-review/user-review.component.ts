a253fc102cea4b892c509d20915a4b463e8d4ffb
Diego-monfort-landot <diegomonfort1989@gmail.com>
2025-10-16 09:46:24 +0200

    fix(user-review): evita error SSR al inicializar Keen Slider solo en entorno navegador
    - Añade verificación con isPlatformBrowser para evitar acceso a document en renderizado del servidor
    - Protege la inicialización de Keen Slider dentro de ngAfterViewInit con comprobación de plataforma
    - Corrige ReferenceError: document is not defined en Angular Universal
    - Mejora compatibilidad con SSR sin afectar comportamiento en cliente
    
    fixes #keen-slider-ssr

--- src/app/user-review/user-review.component.ts
+++ src/app/user-review/user-review.component.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import KeenSlider from 'keen-slider';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+
 
 
 @Component({
@@ -12,44 +15,47 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './user-review.component.html',
   styleUrls: ['./user-review.component.scss']
 })
+
 export class UserReviewComponent implements OnInit, AfterViewInit {
   @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
   slider: any = null;
   users: any[] = [];
 
-constructor(private http: HttpClient) {}
-
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   ngOnInit(): void {
-  this.http.get<any>('https://randomuser.me/api/?results=6&nat=es')
-    .subscribe((res: any) => {
-      this.users = res.results.map((user: any) => ({
-        name: `${user.name.first} ${user.name.last}`,
-        city: user.location.city,
-        photo: user.picture.large,
-        rating: Math.floor(Math.random() * 3) + 3
-      }));
-    });
-}
+    this.http.get<any>('https://randomuser.me/api/?results=6&nat=es')
+      .subscribe((res: any) => {
+        this.users = res.results.map((user: any) => ({
+          name: `${user.name.first} ${user.name.last}`,
+          city: user.location.city,
+          photo: user.picture.large,
+          rating: Math.floor(Math.random() * 3) + 3
+        }));
+      });
+  }
 
-ngAfterViewInit(): void {
-  setTimeout(() => {
-    if (this.sliderRef?.nativeElement) {
-      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
-        loop: true,
-        slides: { perView: 1, spacing: 15 },
-        breakpoints: {
-          '(min-width: 540px)': { slides: { perView: 2, spacing: 10 } },
-          '(min-width: 1024px)': { slides: { perView: 3, spacing: 20 } }
-        },
-        created: (slider) => {
-          setInterval(() => {
-            slider.next();
-          }, 5000); 
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        if (this.sliderRef?.nativeElement) {
+          this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+            loop: true,
+            slides: { perView: 1, spacing: 15 },
+            breakpoints: {
+              '(min-width: 540px)': { slides: { perView: 2, spacing: 10 } },
+              '(min-width: 1024px)': { slides: { perView: 3, spacing: 20 } }
+            },
+            created: (slider) => {
+              setInterval(() => slider.next(), 3000);
+            }
+          });
         }
       });
     }
-  });
+  }
 }
 
-}
